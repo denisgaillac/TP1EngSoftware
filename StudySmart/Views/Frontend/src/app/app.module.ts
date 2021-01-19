4a51b98aaaa351e3dec2cbbcbb01ccad0679aa56
lucas.sousa <lucasomsousa@gmail.com>
2021-01-18 22:05:18 -0300

    Inicio navbar

--- StudySmart/Views/Frontend/src/app/app.module.ts
+++ StudySmart/Views/Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DragDropModule } from '@angular/cdk/drag-drop'
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActivitiesComponent
+    ActivitiesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
