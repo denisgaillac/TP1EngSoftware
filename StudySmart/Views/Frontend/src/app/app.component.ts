812eb51e33b2830faf2e8c0b6ca6b5c82e54e429
Fagner Alves <altino.fagner@gmail.com>
2021-01-20 16:59:58 -0300

    componente de desempenho e rotas

--- StudySmart/Views/Frontend/src/app/app.component.ts
+++ StudySmart/Views/Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+
+  constructor(
+    public router: Router
+  ) {}
+
+  navigatePerformance() {
+    this.router.navigateByUrl('/performance');
+  }
+  navigateActivities() {
+    this.router.navigateByUrl('/');
+  }
 }
