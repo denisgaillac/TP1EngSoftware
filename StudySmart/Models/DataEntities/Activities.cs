662ddea2bae974c3da3aff0b3bbab43dff87db99
Denisgaillac <denisgaillac@ufmg.br>
2021-01-24 11:20:21 -0300

    Esqueleto de Classes e interfaces p/ CRUD de Atividades

--- StudySmart/Models/DataEntities/Activities.cs
+++ StudySmart/Models/DataEntities/Activities.cs
@@ -1,5 +1,5 @@
 using System;
-using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StudySmart.Models.DataEntities
