50e01f764554a7d99b25d7d3c84f896c738050bd
Thais <thaisavilam@gmail.com>
2021-01-30 19:42:42 -0300

    CRUD implementado no frontend

--- StudySmart/Views/Frontend/src/app/interfaces/Activity.ts
+++ StudySmart/Views/Frontend/src/app/interfaces/Activity.ts
@@ -1,5 +1,5 @@
 export interface Activity {
-    id: number,
+    id?: number,
     name: string,
     difficulty: Difficulty,
     doneStatus: DoneStatus,
--- StudySmart/Views/Frontend/src/app/services/activities.service.ts
+++ StudySmart/Views/Frontend/src/app/services/activities.service.ts
@@ -1,5 +1,6 @@
-import { Activity } from './../interfaces/Activity';
-import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Activity, DoneStatus } from './../interfaces/Activity';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,7 +8,8 @@ import { Injectable } from '@angular/core';
 })
 export class ActivitiesService {
 
-  baseRoute: string = "https://localhost:5001/api/";
+  baseRoute: string = environment.baseRoute;
+  controller: string = "Activities/";
 
   constructor(
     private httpClient: HttpClient
@@ -15,7 +17,48 @@ export class ActivitiesService {
 
   getAll(){
     return new Promise<Activity[]>((resolve, reject) => {
-      this.httpClient.get<Activity[]>(this.baseRoute + 'Activities')
+      this.httpClient.get<Activity[]>(this.baseRoute + this.controller)
+      .subscribe(data => {
+          data.forEach(act => {
+            act.expirationDate = new Date(act.expirationDate);
+            act.conclusionDate = act.conclusionDate ? new Date(act.conclusionDate) : null;
+          });
+          resolve(data);
+        }, err => {
+          reject(err);
+      });
+    })
+  }
+
+  save(activity: Activity): Promise<Activity>{
+    return new Promise<Activity>((resolve, reject) => {
+      this.httpClient.post<Activity>(this.baseRoute + this.controller + 'CreateActivity', activity)
+      .subscribe(data => {
+          data.expirationDate = new Date(data.expirationDate);
+          data.conclusionDate = data.conclusionDate ? new Date(data.conclusionDate) : null;
+          resolve(data);
+        }, err => {
+          reject(err);
+      });
+    })
+  }
+
+  update(activity: Activity): Promise<Activity>{
+    return new Promise<Activity>((resolve, reject) => {
+      this.httpClient.patch<Activity>(this.baseRoute + this.controller + 'UpdateActivity', activity)
+      .subscribe(data => {
+          data.expirationDate = new Date(data.expirationDate);
+          data.conclusionDate = data.conclusionDate ? new Date(data.conclusionDate) : null;
+          resolve(data);
+        }, err => {
+          reject(err);
+      });
+    })
+  }
+
+  delete(activityId: number){
+    return new Promise<JSON>((resolve, reject) => {
+      this.httpClient.delete<JSON>(this.baseRoute + this.controller + 'DeleteActivity/' + activityId)
       .subscribe(data => {
           resolve(data);
         }, err => {
